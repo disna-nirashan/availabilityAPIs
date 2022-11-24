40e21e540d9d921a9c77aaf61bde1ec7493f3d88
disna <cdisnac@gmail.com>
2022-11-24 12:08:32 +0530

    Initial Commit

---
+++ src/main/java/com/travel/availabilityAPIs/AvailabilityApIsApplication.java
@@ -0,0 +1,14 @@
+package com.travel.availabilityAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailabilityApIsApplication.class, args);
+		System.out.println("availabilityAPIs service running !");
+	}
+
+}
---
+++ src/test/java/com/travel/availabilityAPIs/AvailabilityApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.availabilityAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailabilityApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
