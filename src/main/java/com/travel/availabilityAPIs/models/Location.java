5d238027fc99b1fb81fe97df0a85789cd3255f83
disna <cdisnac@gmail.com>
2022-11-25 15:51:55 +0530

    model added

---
+++ src/main/java/com/travel/availabilityAPIs/models/Location.java
@@ -0,0 +1,50 @@
+package com.travel.availabilityAPIs.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="locationKey")
+    private  int locationKey;
+
+    @Column(name = "locationID")
+    private String locationID;
+
+    @Column(name = "postalCode")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+
+
+    public String getLocationID() {
+        return locationID;
+    }
+
+    public void setLocationID(String locationID) {
+        this.locationID = locationID;
+    }
+
+
+
+
+    public int getLocationKey() {
+        return locationKey;
+    }
+
+    public void setLocationKey(int locationKey) {
+        this.locationKey = locationKey;
+    }
+
+
+
+}
