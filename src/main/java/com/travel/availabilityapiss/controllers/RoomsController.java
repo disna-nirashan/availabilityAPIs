92910a994141293e1795204a7dce3c471b976f97
disna <cdisnac@gmail.com>
2022-11-30 22:23:48 +0530

    Finalized

--- src/main/java/com/travel/availabilityapiss/controllers/RoomsController.java
+++ src/main/java/com/travel/availabilityapiss/controllers/RoomsController.java
@@ -9,19 +9,18 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import javax.validation.constraints.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-
 
 
 @RestController
@@ -36,66 +35,66 @@ public class RoomsController {
 
     @GetMapping("/availability")
     @ApiOperation(value = "Find hotel rooms availability for your details"
-    ,notes = "Provide relevent correct data "
+            , notes = "Provide relevent correct data "
 
     )
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved Data",response = SearchResponse.class),
+            @ApiResponse(code = 200, message = "Successfully retrieved Data", response = SearchResponse.class),
             @ApiResponse(code = 404, message = "Object Not found "),
             @ApiResponse(code = 400, message = "Bad Request "),
             @ApiResponse(code = 500, message = "Internal Server Error")
     })
 
-    public ResponseEntity<SearchResponse>  getRoomsAvailability (
-            @ApiParam(name = "LocationKey", value = "Location Key of the relevant Location", example = "1") @NotNull @RequestParam(value ="LocationKey") Integer locationKey,
+    public ResponseEntity<SearchResponse> getRoomsAvailability(
+            @ApiParam(name = "LocationKey", value = "Location Key of the relevant Location", example = "1") @NotNull @RequestParam(value = "LocationKey") Integer locationKey,
             @ApiParam(name = "AdultCount", value = "Adult Count of the Passengers", example = "1") @NotNull @Min(1) @RequestParam(value = "AdultCount") Integer adultCount,
-            @ApiParam(name = "ChildCount", value = "Child Count of the Passengers", example = "1") @NotNull @Min(0) @RequestParam(value = "ChildCount") Integer infantCount ,
-            @ApiParam(name = "FromDate", value = "Check in Date", example ="2022-12-10 00:00:00" )  @NotNull @RequestParam(value = "FromDate") Timestamp fromDate ,
-            @ApiParam(name = "ToDate", value = "Check out Date", example ="2022-12-11 00:00:00" ) @NotNull @RequestParam(value = "ToDate") Timestamp toDate
-    ){
-        var loc=locationRepo.findBylocationKey(locationKey);
-        if(loc==null){
+            @ApiParam(name = "ChildCount", value = "Child Count of the Passengers", example = "1") @NotNull @Min(0) @RequestParam(value = "ChildCount") Integer infantCount,
+            @ApiParam(name = "FromDate", value = "Check in Date", example = "2022-12-10 00:00:00") @NotNull @RequestParam(value = "FromDate") Timestamp fromDate,
+            @ApiParam(name = "ToDate", value = "Check out Date", example = "2022-12-11 00:00:00") @NotNull @RequestParam(value = "ToDate") Timestamp toDate
+    ) {
+        var loc = locationRepo.findBylocationKey(locationKey);
+        if (loc == null) {
 
-            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError("LocationKey not found in system !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)),HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError("LocationKey not found in system !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)), HttpStatus.NOT_FOUND);
         }
-        var fromDateCheck=checkDatewithCurrentDate(fromDate,"FromDate");
-        if(fromDateCheck!=null){
+        var fromDateCheck = checkDatewithCurrentDate(fromDate, "FromDate");
+        if (fromDateCheck != null) {
             return fromDateCheck;
         }
-        var toDateCheck=checkDatewithCurrentDate(toDate,"ToDate");
-        if(toDateCheck!=null){
+        var toDateCheck = checkDatewithCurrentDate(toDate, "ToDate");
+        if (toDateCheck != null) {
             return toDateCheck;
         }
-        var toFromDatesCheck=checkfromDateAndToDate(fromDate,toDate);
-        if(toFromDatesCheck!=null){
+        var toFromDatesCheck = checkfromDateAndToDate(fromDate, toDate);
+        if (toFromDatesCheck != null) {
             return toFromDatesCheck;
         }
 
 
         SearchResponse returnResponce;
-        try{
-            returnResponce =roomsService.getRoomsAvailability(locationKey,adultCount,infantCount,fromDate,toDate);
-            return new ResponseEntity<>(returnResponce ,HttpStatus.OK);
-        }catch(RuntimeException e){
-            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError("Internal Server Error !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)),HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            returnResponce = roomsService.getRoomsAvailability(locationKey, adultCount, infantCount, fromDate, toDate);
+            return new ResponseEntity<>(returnResponce, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError("Internal Server Error !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)), HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
 
     }
 
-    private ResponseEntity<SearchResponse> checkDatewithCurrentDate(Timestamp checkingDat,String fieldName){
+    private ResponseEntity<SearchResponse> checkDatewithCurrentDate(Timestamp checkingDat, String fieldName) {
 
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd 00:00:00.000").format(new java.util.Date());
         var currentTs = Timestamp.valueOf(timeStamp);
-        if(currentTs.compareTo(checkingDat)>0){
-            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError(fieldName+" can't be a past Date !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)), HttpStatus.BAD_REQUEST);
+        if (currentTs.compareTo(checkingDat) > 0) {
+            return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError(fieldName + " can't be a past Date !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)), HttpStatus.BAD_REQUEST);
 
         }
         return null;
     }
 
-    private ResponseEntity<SearchResponse> checkfromDateAndToDate(Timestamp fromDate,Timestamp toDate){
-        if(fromDate.compareTo(toDate)>0){
+    private ResponseEntity<SearchResponse> checkfromDateAndToDate(Timestamp fromDate, Timestamp toDate) {
+        if (fromDate.compareTo(toDate) > 0) {
             return new ResponseEntity<>(SearchResponse.create().setErrorData(ErrorData.create().setError("ToDate can't be a past than FromDate !")).setMetaData(ResponeMetaData.create().setStatus(FAILED_STATUS)), HttpStatus.BAD_REQUEST);
 
         }
--- src/main/java/com/travel/availabilityapiss/repositoryies/AllocationRepository.java
+++ src/main/java/com/travel/availabilityapiss/repositoryies/AllocationRepository.java
@@ -10,14 +10,13 @@ import org.springframework.data.repository.query.Param;
 import java.sql.Timestamp;
 import java.util.List;
 
-public interface AllocationRepository extends JpaRepository<Allocation,Integer> {
-
+public interface AllocationRepository extends JpaRepository<Allocation, Integer> {
 
 
     @Query("select h.hotelID as hotel ,rt.roomTypeID as roomType ,rt.adultCount as adultCount ,rt.infantCount as childCount,(r.roomsCount-COALESCE(a.allocatedCount,0)) as noOfAvailableRooms,rr.rate as roomRate from Rooms r inner join  Hotel h on r.hotelKey=h.hotelKey inner join Location l on h.locationKey=l.locationKey inner join RoomType rt on r.roomTypeKey=rt.roomTypeKey inner join RoomsRate rr on (rr.roomsKey=r.roomsKey and :fromDate between rr.startDate and rr.endDate) left outer join Allocation a on a.roomsKey=r.roomsKey AND a.allocatedCount=(select max(al.allocatedCount) from Allocation al where al.roomsKey=a.roomsKey AND ((al.startDate>= :fromDate AND al.startDate <=:toDate) OR (al.endDate<=:toDate AND al.endDate >=:fromDate) OR   (al.startDate<= :fromDate  AND al.endDate >=:fromDate  )   OR (al.startDate <= :toDate  AND al.endDate >= :toDate ))  group by al.roomsKey) where l.locationKey=:locationKey and r.roomTypeKey in :roomTypes")
-            
-            
+
+
 //    @Query("select a from Allocation a where a.allocatedCount=(select   max(al.allocatedCount) from Allocation al where al.roomsKey=a.roomsKey group by al.roomsKey) AND(  (a.startDate<= :fromDate AND a.endDate >=:fromDate) OR (a.startDate <= :toDate AND a.endDate >= :toDate))")
-    List<SearchQueryResult> finalResult(@Param("roomTypes") List<Integer> roomTypeList ,@Param("locationKey") int locationKey ,@Param("fromDate") Timestamp fromDate ,@Param("toDate") Timestamp toDate);
+    List<SearchQueryResult> finalResult(@Param("roomTypes") List<Integer> roomTypeList, @Param("locationKey") int locationKey, @Param("fromDate") Timestamp fromDate, @Param("toDate") Timestamp toDate);
 
 }
