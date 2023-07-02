8569bcb8d4baf8e02ee766e5308cce22c9d1370b
chandika <zchandikaz@gmail.com>
2023-07-02 22:33:16 +0530

    docker changes

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ mvn clean package
 docker build -f Dockerfile -t avail-api:latest .
 
 docker run --env-file env-vars-local -p 127.0.0.1:9090:8080 avail-api:latest
+
+#myharbor.test.com
+#docker tag avail-api:latest myharbor.test.com/test/avail-api:latest
+#docker push myharbor.test.com/test/avail-api:latest
